6c4e5c5a88b7e9d4b5a257c02a250b32863dd8fd
서연희 <tjdusgml1219@naver.com>
2025-05-23 11:03:23 +0900

    전체urls추가

--- pjt-back/pjt_back/urls.py
+++ pjt-back/pjt_back/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('articles/', include('articles.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     # path('exchange/', include('exchange.urls')),
     path('products/', include('financial_products.urls')),
-]
+     # TokenAuthentication
+     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    # Registration 
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
