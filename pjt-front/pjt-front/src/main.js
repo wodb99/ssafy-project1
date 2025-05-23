a5c6b0c954de93de8e1141066d107843f3ce2a14
서연희 <tjdusgml1219@naver.com>
2025-05-23 16:32:28 +0900

    유튜브검색

--- pjt-front/pjt-front/src/main.js
+++ pjt-front/pjt-front/src/main.js
@@ -1,22 +1,18 @@
-
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-
+// Pinia 생성 및 플러그인 적용
 const pinia = createPinia()
-
 pinia.use(piniaPluginPersistedstate)
-// app.use(createPinia())
+
+// 전역 등록
 app.use(pinia)
 app.use(router)
-
 
 app.mount('#app')
